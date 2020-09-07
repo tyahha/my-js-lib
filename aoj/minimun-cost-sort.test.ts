96c53ad4ccb2627d15a1c114a4e3841527669f56
toshinori sato <tyahhahha@gmail.com>
2020-09-07 23:39:03 +0900

    solve minimum cost sort

--- aoj/minimun-cost-sort.test.ts
+++ aoj/minimun-cost-sort.test.ts
@@ -4,8 +4,14 @@ import { getMinimumCostOfSort } from "./minimun-cost-sort"
 describe("minimum cost sort", () => {
   test("test1", () => {
     expect(getMinimumCostOfSort(ssvToNums("1 5 3 4 2"))).toBe(7)
+  })
+  test("test2", () => {
     expect(getMinimumCostOfSort(ssvToNums("4 3 2 1"))).toBe(10)
+  })
+  test("test3", () => {
     expect(getMinimumCostOfSort(ssvToNums("6 3 4 2 1 5"))).toBe(24)
+  })
+  test("test4", () => {
     expect(getMinimumCostOfSort(ssvToNums("10 7 8 9"))).toBe(48)
   })
 })
--- aoj/minimun-cost-sort.ts
+++ aoj/minimun-cost-sort.ts
@@ -1,38 +1,72 @@
 import { quickSort } from "./quick-sort"
 import { Ordering } from "./common"
 
-interface WithIndex<T> {
+interface DataForCost<T> {
   value: T
-  index: number
+  originalIndex: number
+  sortedIndex: number
+  used: boolean
 }
 
-const withIndex = <T>(value: T, index: number) => ({
+const createData = <T>(value: T, originalIndex: number): DataForCost<T> => ({
   value,
-  index,
+  originalIndex,
+  sortedIndex: 0,
+  used: false,
 })
 
 export const getMinimumCostOfSort = (a: ReadonlyArray<number>): number => {
   const A = a.concat()
-  const indexedArray = a.map((v, i) => withIndex(v, i))
+  const n = A.length
+  const originalArray = a.map((v, i) => createData(v, i))
 
-  const sortedArray = quickSort(indexedArray, (a, b) =>
+  const sortedArray = quickSort(originalArray, (a, b) =>
     a.value > b.value
       ? Ordering.GT
       : a.value < b.value
       ? Ordering.LT
       : Ordering.EQ
-  )
+  ).map((e, i) => {
+    e.sortedIndex = i
+    return e
+  })
+
+  const minValue = sortedArray[0].value
 
   let cost = 0
-  for (let i = sortedArray.length - 1; i >= 0; i--) {
-    if (sortedArray[i].index !== i) {
-      const e1 = indexedArray[sortedArray[i].index]
-      const e2 = indexedArray[i]
-      cost += e1.value + e2.value
-      const tmp = e1.index
-      e1.index = e2.index
-      e2.index = tmp
+  for (let i = 0; i < n; i++) {
+    const cur = sortedArray[i]
+    if (cur.used) continue
+
+    cur.used = true
+
+    if (cur.originalIndex === i) continue
+
+    // グループを抽出する
+    let groupMember = originalArray[i]
+    let groupValueSum = cur.value
+    let groupMinValue = cur.value
+    let groupMemberCount = 1
+
+    while (true) {
+      groupMember.used = true
+      groupValueSum += groupMember.value
+      groupMinValue = Math.min(groupMinValue, groupMember.value)
+      groupMemberCount++
+      groupMember = originalArray[groupMember.sortedIndex]
+      if (i === groupMember.sortedIndex) {
+        break
+      }
     }
+
+    const costWithGroupMinValue =
+      groupValueSum + groupMinValue * (groupMemberCount - 2)
+    const costWithMinValue =
+      (groupMinValue + minValue) * 2 +
+      (groupValueSum - groupMinValue) +
+      minValue * (groupMemberCount - 1)
+
+    cost += Math.min(costWithGroupMinValue, costWithMinValue)
   }
 
   return cost
